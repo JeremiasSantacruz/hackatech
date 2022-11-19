5a904300b8adfc635ca20106befaf2197a7d4f01
jeremias santacruz <jeremias.santacruz@proveedor.ues21.edu.ar>
2022-11-19 15:10:38 -0300

    Intit backend

---
+++ backEnd/src/main/java/org/poloit/hsckstech/HsckstechApplication.java
@@ -0,0 +1,13 @@
+package org.poloit.hsckstech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsckstechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsckstechApplication.class, args);
+    }
+
+}
---
+++ backEnd/src/test/java/org/poloit/hsckstech/HsckstechApplicationTests.java
@@ -0,0 +1,13 @@
+package org.poloit.hsckstech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsckstechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
